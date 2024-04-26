3cf6fd3a38ba149cb388229b1bf717558fccd0aa
manyi zhao <zhaomanyi2005@gmail.com>
2024-04-26 04:09:47 -0700

    Update Stream.java

--- practice/src/open_cv/Stream.java
+++ practice/src/open_cv/Stream.java
@@ -1,23 +1,40 @@
 package open_cv;
 
 import org.opencv.core.Core;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.core.Mat;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.time.StopWatch;
+import uwcse.io.Input;
 
-import static org.opencv.highgui.HighGui.imshow;
+import javax.imageio.ImageIO;
+
+import static org.opencv.highgui.HighGui.*;
 
 public class Stream {
 
     // https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
-    VideoCapture cam;
-    StopWatch timer;
-    Mat frame;
+    private VideoCapture cam;
+    private StopWatch timer;
+    private Mat frame;
+
+    private int filter;
+    public static int maxMode;
+
+    final String winName = "frame";
+
 
     //for some reason it takes forever to load the camera - around 17 seconds on my pc
     public Stream() {
@@ -28,28 +45,58 @@ public class Stream {
         cam = new VideoCapture(0);
 
         frame = new Mat();
+        namedWindow(winName);
         openCamera();
     }
 
-    public void displayFrame(){
-        isFrameValid();
-        imshow("frame", frame);
-    }
+//    public void displayFrame(){
+//        isFrameValid();
+//        destroyAllWindows();
+//        imshow(winName, frame);
+//        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2HSV);
+//        waitKey();
+//    }
 
     public Mat getFrame(){
         isFrameValid();
         return frame;
     }
 
-    private boolean isFrameValid(){
+    // returns an image for JFrame
+    public BufferedImage getImage(){
+
+        Mat img = getFrame(); // updates img
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", img, matOfByte);
+        byte[] imgBytes = matOfByte.toArray();
+
+        BufferedImage bufferedImage;
+        InputStream in = new ByteArrayInputStream(imgBytes);
+        try {
+            bufferedImage = ImageIO.read(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bufferedImage;
+
+    }
+
+    public int getFilter(){
+        return filter % maxMode;
+    }
+
+
+    private void isFrameValid(){
+        frame = new Mat();
         boolean checkFrame = cam.read(frame);
         if (!checkFrame){
             System.out.println("Error reading frame");
             close();
         }
-        return checkFrame;
+        CameraMode c = new CameraMode();
+        c.filter(frame, 2);
     }
-
 
     // checks for a valid camera connection
     public void openCamera() {
@@ -64,10 +111,16 @@ public class Stream {
     // shuts off the camera
     public void close(){
         cam.release();
+        destroyAllWindows();
         System.out.println("cam closed");
     }
 
     public boolean isOpened() {
         return cam.isOpened();
     }
+
+    public void setFilter(int filter){
+        this.filter = filter;
+    }
+
 }
